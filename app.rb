f2db52f2aef2d2029e16339492e085bdee4a8bf7
github username <github email address>
2018-08-28 16:57:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
 
     # code other routes/actions here
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = params[:pirate]
+      #binding.pry
+      erb :"pirates/show"
+    end
 
   end
 end
