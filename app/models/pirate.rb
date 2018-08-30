b2b557ed223b0570ba1bbd847b0d2d9dc9866f15
mguevara717 <marissa.guevara717@gmail.com>
2018-08-29 21:29:50 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,24 @@
 class Pirate
-end
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end#END OF CLASS
+
+# The ship class should have name, type, and booty attributes, as well as a class method .all
+# that returns all the ships and a class method .clear
+# that deletes all ships.
