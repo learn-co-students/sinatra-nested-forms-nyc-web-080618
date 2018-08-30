9c1fd33a54557de9d8977fff4c1d43e62726eb91
github username <ariellenramirez@gmail.com>
2018-08-29 22:29:31 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
+
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:weight]
+    @booty = args[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @pirates
+  end
+
 end
